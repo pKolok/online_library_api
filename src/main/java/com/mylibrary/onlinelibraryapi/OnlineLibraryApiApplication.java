c5a9222be63eb6e7fef219a0488a391b92f1a262
panousias <panousias@hotmail.com>
2025-02-18 20:43:57 +0200

    Set up project

---
+++ src/main/java/com/mylibrary/onlinelibraryapi/OnlineLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.mylibrary.onlinelibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLibraryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mylibrary/onlinelibraryapi/OnlineLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylibrary.onlinelibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
